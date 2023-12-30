139e7dd4afebbf683a2713c3320923767bc0581c
Komal Shivangi Vyas <77508110+KomalVyas23@users.noreply.github.com>
2023-12-31 00:16:51 +0530

    Update List.jsx

--- ToDo/src/components/List.jsx
+++ ToDo/src/components/List.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 function List(props) {
-    const {tasksArr} = props;
+    const {tasksArr,handleDelete}=props;
+    console.log("list is rendered")
+    
   return (
     <div className='list'>
-    <ul>
-     {tasksArr.map(task=><li>{task}</li>)}
-    </ul>
- </div>
+            <ul>
+                {tasksArr.map((task,idx)=><li onClick={()=>handleDelete(idx)} key={idx}>{task}</li>)}
+            </ul>
+        </div>
   )
 }
 
