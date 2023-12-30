9c69940b08e5dc196c46956d7136ec0a39dc29d7
Komal Shivangi Vyas <77508110+KomalVyas23@users.noreply.github.com>
2023-12-31 00:16:23 +0530

    Update Todo.jsx

--- ToDo/src/components/Todo.jsx
+++ ToDo/src/components/Todo.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import { useState } from 'react';
+import {useState} from 'react'
 import InputBox from './InputBox';
 import List from './List';
 
-function Todo(){
-   
-    const [tasksArr,setTasksArr] = useState([]);
-   
-    const handleTask=()=>{
-        let newTasksArr=[...tasksArr,inputValue];
-        setTasksArr(newTasksArr);
-        setValue("");
+function Todo() {
+    
+    const [tasksArr,setTasksArr]=useState([]);
+    console.log("todo is rendered")
+    const handleTask=(inputValue)=>{
+        let newTaskArr=[...tasksArr,inputValue]
+        console.log(newTaskArr);
+        setTasksArr(newTaskArr);
     }
-    return(
-        <div>
-          <InputBox fn={handleTask}/> 
-           <List tasksArr={tasksArr} /> 
-        </div>
-    )
+
+    const handleDelete=(id)=>{
+      console.log("delete this task");
+      //filter out the task with id 5 , and show the rest of the tasks . 
+      let remainingTasks=tasksArr.filter((task,idx)=>idx!=id)
+      // console.log(remainingTasks);
+      setTasksArr(remainingTasks);
+    }
+
+  return (
+    <div>
+        <InputBox handleTask={handleTask}></InputBox>
+        <List tasksArr={tasksArr} handleDelete={handleDelete}></List>
+    </div>
+  )
 }
+
 
 export default Todo
