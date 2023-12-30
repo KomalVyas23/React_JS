9dab6513ee55ac9dfcc1db96e62a7bc40997f0d8
Komal Shivangi Vyas <77508110+KomalVyas23@users.noreply.github.com>
2023-12-31 00:15:59 +0530

    Update InputBox.jsx

--- ToDo/src/components/InputBox.jsx
+++ ToDo/src/components/InputBox.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import {useState} from 'react'
 
 function InputBox(props) {
-    const {handleTask} = props;
-    const [inputValue,setValue] = useState("");
+    const {handleTask}=props;
+
+    const [inputValue,setValue]=useState("");
     const handleValue=(e)=>{
         setValue(e.target.value);
     }
+
+    const handleAdd=()=>{
+      handleTask(inputValue)
+      setValue("");
+    }
+
+    console.log("IB is rendered")
+
   return (
     <div className='inputBox'>
-                <input type="text" placeholder='Enter Your Tasks...' value={inputValue} onChange={handleValue} />
-                <button onClick={handleTask()}>Add Task</button>
-            </div>
+            <input type="text" placeholder='Enter you Tasks....' value={inputValue} onChange={handleValue}></input>
+            <button onClick={handleAdd}>Add Task</button>
+        </div>
   )
 }
 
